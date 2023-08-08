f9322cbd1dd6edbfb248c6a12a75ba291923aadf
ElayQ <vadimgazizovq@gmail.com>
2023-08-08 14:24:08 +0500

    Update DB

--- DAL/ApplicationDbContext.cs
+++ DAL/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+        Database.EnsureCreated();
     }
     
     public DbSet<Employee> Employees { get; set; }
